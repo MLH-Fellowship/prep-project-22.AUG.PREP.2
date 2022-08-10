90eea1c333fdf39c15400ff15f37a78c7d492b49
jcyling <jane.cy.ling@gmail.com>
2022-08-10 18:16:55 +0100

    Locate user via geolocation API

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,6 +5,7 @@ import ErrorComponent from "./ErrorComponent";
 import ResultsComponent from "./ResultsComponent";
 import SearchComponent from "./SearchComponent";
 import RequiredItems from "./RequiredItems";
+import useLocation from "../Hooks/useLocation";
 import Map from "./Map";
 
 export default function App() {
@@ -13,6 +14,26 @@ export default function App() {
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
 
+  const geolocateUser = useLocation();
+
+  // Fetch data based on geolocation
+  useEffect(() => {
+    if (geolocateUser.length !== 0) {
+      fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${geolocateUser[0]}&lon=${geolocateUser[1]}&limit=1&appid=${process.env.REACT_APP_APIKEY}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setCity(result[0].name);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }
+  }, [geolocateUser]);
+
+  // Fetch data based on user input
   useEffect(() => { // weather
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
       .then(res => res.json())
