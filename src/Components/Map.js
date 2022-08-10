be29edf84e17f70f0b79534533170a4a93969114
Ahmedsaed <ahmedsaed2652003@gmail.com>
2022-08-10 18:45:45 +0200

    Code refactoring and optimizations

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -9,49 +9,44 @@ export default function Map({ setIsLoaded, setResults, setError }) {
     const [lng, setLng] = useState(-70.9);
     const [lat, setLat] = useState(42.35);
     const [zoom, setZoom] = useState(9);
-    const [markers, setMarkers] = useState([]);
+    const [marker, setMarker] = useState();
 
     useEffect(() => { // mapbox
-    if (map.current) return; // initialize map only once
+        if (map.current) return; // initialize map only once
 
-    map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [lng, lat],
-        zoom: zoom
-    });
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
 
-    map.current.on('click', (e) => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${e.lngLat.lat}&lon=${e.lngLat.lng}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
-        .then(res => res.json())
-        .then(
-        (result) => {
-            if (result['cod'] !== 200) {
-            setIsLoaded(false)
-            } else {
-            setMarkers(markers => [...markers, 
-                new mapboxgl.Marker()
-                .setLngLat([e.lngLat.lng, e.lngLat.lat])
-                .addTo(map.current)]);
-
-            setIsLoaded(true);
-            setResults(result);
-            }
-        },
-        (error) => {
-            setIsLoaded(true);
-            setError(error);
-        }
-        )
-    });
+        map.current.on('click', (e) => {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${e.lngLat.lat}&lon=${e.lngLat.lng}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    if (result['cod'] !== 200) {
+                    setIsLoaded(false)
+                    } else {
+                        setMarker(marker => {
+                            if (marker) markers.remove()
+                            return new mapboxgl.Marker()
+                                    .setLngLat([e.lngLat.lng, e.lngLat.lat])
+                                    .addTo(map.current)
+                        })
+                        
+                        setIsLoaded(true);
+                        setResults(result);
+                    }
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+        });
     }, []);
-
-    useEffect(() => {
-        console.log(markers);
-        if (markers.length > 1) { // if marker already created
-            markers[markers.length - 2].remove(); // remove second to last marker
-        }
-    }, [markers])
 
     return (
         <>
