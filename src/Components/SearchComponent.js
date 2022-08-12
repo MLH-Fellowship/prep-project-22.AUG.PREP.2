75b56476e702e8e3f7e977375a70bc9e3b0ad37b
Yuehan Wang <yuehan@uchicago.edu>
2022-08-12 12:38:56 -0500

    bugs remain

--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -1,10 +1,54 @@
+import React from "react";
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng
+  } from "react-places-autocomplete";
 
-export default function SearchComponent({city, changeCity}) {
-
+export default function SearchComponent({}) {
+    const [city, setCity] = React.useState("");
+    const [coordinates, setCoordinates] = React.useState({
+    lat: null,
+    lng: null
+    });
+    const handleSelect = async value => {
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng(results[0]);
+        setCity(value);
+        setCoordinates(latLng);
+      };
+      
     return (
-        <input
-          type="text"
-          value={city}
-          onChange={event => changeCity(event.target.value)} />
+        <div>
+      <PlacesAutocomplete
+        value={city}
+        onChange={setCity}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.lng}</p>
+
+            <input {...getInputProps({ placeholder: "Type city" })} />
+
+            <div>
+              {loading ? <div>...loading</div> : null}
+
+              {suggestions.map(suggestion => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
+                };
+
+                return (
+                  <div {...getSuggestionItemProps(suggestion, { style })}>
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    </div>
     )
 }
