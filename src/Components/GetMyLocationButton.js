12fbc2140cfc23ea036c9160e6ffd2f26b92fe5f
Ahmedsaed <ahmedsaed2652003@gmail.com>
2022-08-19 15:09:49 +0200

    replace get-loc btn text with icon

--- src/Components/GetMyLocationButton.js
+++ src/Components/GetMyLocationButton.js
@@ -1,5 +1,9 @@
+import pinIcon from "../assets/img/pin.png";
+
 export default function SearchMyLocationButton({getUserLocation}) {
     return (
-      <button onClick={getUserLocation} className="get-user-loc-btn">Get My Location</button>
+      <button onClick={getUserLocation} className="get-user-loc-btn">
+        <img src={pinIcon} className="get-usr-loc-icon" />
+      </button>
     )
   }
